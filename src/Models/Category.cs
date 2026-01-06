e93c3c76f587141e763f78d061a3c6fe4f7f50f4
Caio Santos <caiosantos.dev@outlook.com>
2026-01-06 18:37:11 -0300

    feat: add novas models

---
+++ src/Models/Category.cs
@@ -0,0 +1,16 @@
+using api_infor_cell.src.Models.Base;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace api_infor_cell.src.Models
+{
+    public class Category : ModelBase
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
